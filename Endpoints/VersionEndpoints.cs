ff1040831d9e0b1b0f906e993754c66a38f3d9f8
Fergal Moran <fergal.moran@gmail.com>
2025-10-05 23:33:18 +0100

    Add version endpoint

--- Endpoints/VersionEndpoints.cs
+++ Endpoints/VersionEndpoints.cs
@@ -5,7 +5,7 @@ namespace Xtreamium.Proxy.Endpoints;
 public static class VersionEndpoints {
   public static void RegisterVersionEndpoints(this IEndpointRouteBuilder app) {
     app.MapGet("/version", () => {
-      var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "Unknown";
+      var version = $"v{Assembly.GetExecutingAssembly().GetName().Version?.ToString(3)}" ?? "Unknown";
       return Results.Text(version, "text/plain");
     });
   }
